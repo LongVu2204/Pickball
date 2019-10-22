ab720d5c41ae9a7afe3a82280fb089a409879c14
trandinhnhan0612 <trandinhnhan9958@gmail.com>
2019-10-22 15:05:57 +0700

    Pickball

---
+++ Game1/Game1/Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    class Controller
+    {
+        private Board Game;
+        private Player P1, P2;
+
+        public Controller()
+        {
+            P1 = new Player("Long");
+            P2 = new Player("Nhii");
+            Game = new Board(3, 5, 7);
+        }
+
+        public void PlayGame()
+        {
+            Game.PrintGame();
+            while (true)
+            {
+                Console.WriteLine(P1.GetName() + " turn ");
+                P1.Pickball(Game);
+                Game.PrintGame();
+                if (Game.IsGameOver())
+                {
+                    Console.WriteLine(P1.GetName() + " Loses ");
+                    Console.WriteLine(P2.GetName() + " Wins ");
+                    break;
+                }
+                Console.WriteLine(P2.GetName() + " a turn ");
+                P2.Pickball(Game);
+                if (Game.IsGameOver())
+                {
+                    Console.WriteLine(P2.GetName() + " Loses ");
+                    Console.WriteLine(P1.GetName() + " Wins ");
+                    break;
+                }            
+            }
+        }
+    }
+}
---
+++ Game1/Game1/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    class Player
+    {
+        private string Name;
+
+        public void Pickball(Board game)
+        {
+            Console.Write("Which group do you want to choose? : ");
+            int group = int.Parse(Console.ReadLine());
+            Console.Write("How many balls do you want to pick? : ");
+            int balls = int.Parse(Console.ReadLine());
+            game.Pickball(group, balls);
+        }
+
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+    }
+}
